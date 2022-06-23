35561e6db0abf2d6619ad2e4d50e500cbc81c1b5
DrNickker <mintagirov2015@yandex.ru>
2022-06-23 17:25:55 +0500

    1

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,36 +1,53 @@
-let element = document.getElementsByClassName('modal_active')[0];
-console.log (element)
+
+let element = document.getElementsByClassName('modal_active');
+for (var i = 0; i < modal_active.length; i++) {
+    modal_active[i].addEventListener('click', changeClass, false);
+}
+
+function changeClass() {
+    $(this).toggleClass('modal_success');
+    $('.modal_active').not(this).removeClass('modal_success');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* let element = document.getElementsByClassName('modal_active')[0];
+
+
+function change() {
+    element.width =100;
+    element.height =100;  
+};  
+
+    img.onclick = change;
+
+
+
+let success = document.getElementById('modal_success');
+
+success.onclick = function () {
+    success.className = "modal_success";
+   
+}
 
 function f1 (e) {
    
 
-
-
-
-
-    
 }
 
 element.addEventListener("click", f1)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ */
 
 /* element[0].onclick = function () {
     element[0].className = "modal";
